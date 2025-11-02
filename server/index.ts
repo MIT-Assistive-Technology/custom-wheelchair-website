8de2e1f057dc776cef50ceefd352fd854713d68a
Fiete <g.fiete@FACFVFF48S0Q6L7.tld>
2025-11-02 16:40:56 -0500

    updated index.ts

--- server/index.ts
+++ server/index.ts
@@ -60,12 +60,19 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
+
+  const PORT = parseInt(process.env.PORT || "5173", 10);
+  console.log("Environment port:", process.env.PORT);
+  server.listen(PORT, "127.0.0.1", () => {
+    console.log(`Server running at http://localhost:${PORT}`);
   });
+
+  // const port = parseInt(process.env.PORT || '5000', 10);
+  // server.listen({
+  //   port,
+  //   host: "0.0.0.0",
+  //   reusePort: true,
+  // }, () => {
+  //   log(`serving on port ${port}`);
+  // });
 })();
